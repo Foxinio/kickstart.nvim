ae93ac5c60a652a65db663fbe36f294eb5ff7006
Szymon Jedras <szymon.jedras.wr@gmail.com>
2024-02-16 19:27:02 +0100

    removing unnessesary copies

--- lua/custom/my/style.lua
+++
@@ -1,8 +0,0 @@
-" Color scheme (terminal)
-set t_Co=256
-set background=dark
-colorscheme slate
-" hi LineNr ctermfg=grey ctermbg=darkgrey guifg=grey guibg=darkgrey
-
-hi PmenuThumb ctermfg=white ctermbg=black guifg=white guibg=black
-hi CocFloatThumb ctermfg=white ctermbg=black guifg=white guibg=black
