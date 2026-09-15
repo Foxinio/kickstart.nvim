874833a83ce2d3b32e9f18bb46f55a60298c5d35
Szymon Jędras <sjedras@antmicro.com>
2026-09-15 10:39:42 +0200

    Minor <leader>tx fix

--- lua/plugin-utils/nvim-tree.lua
+++ lua/plugin-utils/nvim-tree.lua
@@ -4,8 +4,14 @@ function M.find_current_file()
 	local bufnr = vim.api.nvim_get_current_buf()
 	local file = vim.api.nvim_buf_get_name(bufnr)
 
-	if file == "" or not vim.fs.relpath(vim.fn.getcwd(), file) then
-		vim.notify("Current file is not under cwd", vim.log.levels.INFO)
+	if file == "" then
+		vim.notify("Cannot find current buffer in nvim-tree: it has no file path", vim.log.levels.INFO)
+		return
+	end
+
+	if vim.fn.filereadable(file) == 0 then
+		vim.notify("Cannot find current buffer in nvim-tree: file does not exist or is not readable: " .. file,
+			vim.log.levels.INFO)
 		return
 	end
 
@@ -13,7 +19,7 @@ function M.find_current_file()
 		buf = bufnr,
 		open = true,
 		focus = true,
-		update_root = false,
+		update_root = true,
 	})
 end
 
